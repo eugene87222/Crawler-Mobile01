73605966cadd6d8496b0bb7c41c5f4df51193fd3
Eugene Yang <eugene87222@gmail.com>
2018-06-21 23:25:50 +0800

    Add files via upload

--- Mobile01Crawler.py
+++ Mobile01Crawler.py
@@ -3,7 +3,9 @@ warnings.filterwarnings('ignore')
 
 import re
 import time
+import sqlite3
 import requests
+from pandas import DataFrame
 from bs4 import BeautifulSoup
 from multiprocessing import Pool
 
@@ -16,109 +18,181 @@ def GetPageContent(url):
     content = BeautifulSoup(res.text)
     return content
 
-# 取得所有文章分類
+#########################################
+# get all topics and save as a txt file #
+# param: url -> url of the home page    #
+#########################################
 def GetAllTopic(url):
-	content = GetPageContent(url)
+    content = GetPageContent(url)
 
-	all_topic = content.find('div', {'id':'top-menu'}).findAll('li')
-	all_topic = [each for each in all_topic if 'topiclist' in each.find('a')['href'] or 'waypointtopiclist' in each.find('a')['href']]
-	# ↑ 取得所有討論群組 (不包含綜合討論區)
-	
-	topic_list = list()
-	for each in all_topic:
-		topic_list.append((each.find('a')['href'], each.find('a').text))
-	
-	return topic_list
+    all_topic = content.find('div', {'id':'top-menu'}).findAll('li')
+    all_topic = [each for each in all_topic if 'topiclist' in each.find('a')['href'] or 'waypointtopiclist' in each.find('a')['href']]
+    
+    file = open('topic_list.txt', 'w')
+    topic_dict = dict()
+    idx = 0
+    for each in all_topic:
+        topic_link = each.find('a')['href']
+        topic_page = GetPageContent('https://www.mobile01.com/' + topic_link)
+        nav = topic_page.findAll('p', {'class':'nav'})[0].text
+        start = nav.find('»')
+        topic_name = nav[start+1:].lstrip().rstrip()
+        while ' » ' in topic_name:
+            topic_name = topic_name.replace(' » ', '>')
+        while ' ' in topic_name:
+            topic_name = topic_name.replace(' ', '')
+
+        topic_dict[str(idx)] = [topic_link, topic_name]
+        file.write(str(idx) + ' ' + topic_link + ' ' + topic_name + '\n')
+        idx += 1
+
+    file.close()
+    return topic_dict
+
+#################################
+# read topic list from txt file #
+#################################
+def ReadTopic():
+    topic_dict = dict()
+    file = open('topic_list.txt', 'r')
+    for line in file:
+        topic = line.replace('\n', '').split(' ')
+        topic_dict[topic[0]] = [topic[1], topic[2]]
+    file.close()
+    return topic_dict
 
 # 取得該分類文章總頁數
 def GetTotalPageNum(url):
-	content = GetPageContent('https://www.mobile01.com/' + url)
-	pagination = content.findAll('div', {'class':'pagination'}) # "[1][2][3]...下一頁"這行
-	if pagination:
-		page_link = pagination[1].findAll('a')
-		if page_link:
-			last_page = page_link[-1]['href']           # 該行的最後一個按鈕就是最後一頁的網址
-			replace = url+'&p='
-			total_page = last_page.replace(replace, '') # 把前面的字串過濾掉，只要網址後面的數字 (頁數)
-		else:
-			total_page = 1
-	else:
-		total_page = -1
-	return total_page
+    content = GetPageContent('https://www.mobile01.com/' + url)
+    pagination = content.findAll('div', {'class':'pagination'}) # "[1][2][3]...下一頁"這行
+    if pagination:
+        page_link = pagination[1].findAll('a')
+        if page_link:
+            last_page = page_link[-1]['href']           # 該行的最後一個按鈕就是最後一頁的網址
+            replace = url+'&p='
+            total_page = last_page.replace(replace, '') # 把前面的字串過濾掉，只要網址後面的數字 (頁數)
+        else:
+            total_page = 1
+    else:
+        total_page = -1
+    return total_page
 
 # 取得從第 1 頁到第 page_num 頁的所有文章網址
 def GetPosts(page_num, url):
-	posts = list()
+    posts = list()
 
-	for i in range(1, 1 + page_num):
-		content = GetPageContent('https://www.mobile01.com/' + url + '&p=' + str(i))
+    for i in range(1, 1 + page_num):
+        content = GetPageContent('https://www.mobile01.com/' + url + '&p=' + str(i))
 
-		table = content.find('table', {'summary':u'文章列表'})
-		rows = table.findAll('tr')[1:]  # 每篇文章連結都是包在 table 的 tr 裡面
-		
-		for row in rows:
-			meta = row.find('a', {'class':'topic_gen'})
-			if meta:
-				link = meta['href']     # 文章網址
-				title = meta.text       # 文章標題
-				reply = row.find('td', {'class':'reply'}).text  # 回覆量
-				detail = row.find('td', {'class':'authur'})
-				detail = detail.findAll('p')
-				date = detail[0].text   # 文章發布日期
-				author = detail[1].text # 文章作者
-				
-				posts.append({
-					'link': link,
-					'title': title,
-					'date': date,
-					'author': author,
-					'reply': reply
-				})
+        table = content.find('table', {'summary':u'文章列表'})
+        rows = table.findAll('tr')[1:]  # 每篇文章連結都是包在 table 的 tr 裡面
+        
+        for row in rows:
+            meta = row.find('a', {'class':'topic_gen'})
+            if meta:
+                link = meta['href']     # 文章網址
+                title = meta.text       # 文章標題
+                reply = row.find('td', {'class':'reply'}).text  # 回覆量
+                detail = row.find('td', {'class':'authur'})
+                detail = detail.findAll('p')
+                date = detail[0].text   # 文章發布日期
+                author = detail[1].text # 文章作者
+                
+                posts.append({
+                    'link': link,
+                    'title': title,
+                    'date': date,
+                    'author': author,
+                    'reply': reply
+                })
 
-	return posts
+    return posts
 
 #################################
 # get the article of the post   #
 # param: url -> url of the post #
 #################################
 def ParseGetArticle(url):
-	soup = GetPageContent('https://www.mobile01.com/' + url)
-	origin = soup.find('div', {'class':'single-post-content'}) # 文章內文在 <div class="single-post-content"> 底下
-	if origin:
-		content = str(origin)
-		# replace <br>, <br\> as a blank line ##################
-		content = re.sub("<br\s*>", "§", content, flags=re.I)  #
-		content = re.sub("<br\s*/>", "§", content, flags=re.I) #
-		content = re.sub("\n+", "§", content, flags=re.I)      #
-		content = re.sub("§+", "\n", content, flags=re.I)      #
-		########################################################
-		content = re.sub("<a\s+[^<>]+>(?P<aContent>[^<>]+?)</a>", "\g<aContent>", content, flags=re.I) # remove hyperlink
-		content = BeautifulSoup(content)
-		div = content.find('div')
-		div.replace_with(div.text)
-		content = content.text.lstrip().rstrip() # 把文章頭尾的空白都移除
-	else:
-		content = 'None'
+    soup = GetPageContent('https://www.mobile01.com/' + url)
+    origin = soup.find('div', {'class':'single-post-content'}) # 文章內文在 <div class="single-post-content"> 底下
+    if origin:
+        content = str(origin)
+        # replace <br>, <br\> and '\n' with a whitespace########
+        content = re.sub("<br\s*>", " ", content, flags=re.I)  #
+        content = re.sub("<br\s*/>", " ", content, flags=re.I) #
+        content = re.sub("\n+", " ", content, flags=re.I)      #
+        ########################################################
+        # remove hyperlink
+        content = re.sub("<a\s+[^<>]+>(?P<aContent>[^<>]+?)</a>", "\g<aContent>", content, flags=re.I)
+        content = BeautifulSoup(content)
+        content = ' '.join(content.text.lstrip().rstrip().split())
+    else:
+        content = 'None'
 
-	return content
+    return content
 
 ##############################################
 # get the articles of each post              #
 # param: post_list -> mata data of all posts #
 ##############################################
 def GetArticles(post_list):
-	post_link = [entry['link'] for entry in post_list]
+    post_link = [entry['link'] for entry in post_list]
 
-	articles = list()
+    articles = list()
 
-	for i in range(len(post_list)):
-		articles.append({
-			'title': post_list[i]['title'], 
-			'link': post_list[i]['link'], 
-			'date': post_list[i]['date'],
-			'author': post_list[i]['author'], 
-			'reply': post_list[i]['reply'], 
-			'content': ParseGetArticle(post_list[i]['link'])
-		})
+    for i in range(len(post_list)):
+        articles.append({
+            'title': post_list[i]['title'], 
+            'link': post_list[i]['link'], 
+            'date': post_list[i]['date'],
+            'author': post_list[i]['author'], 
+            'reply': post_list[i]['reply'], 
+            'content': ParseGetArticle(post_list[i]['link'])
+        })
 
-	return articles
+    return articles
+
+##########################################
+# save data into SQLite database         #
+# param: db_name -> name of the database #
+#        posts -> posts data             #
+##########################################
+def Save2DB(db_name, posts):
+    conn = sqlite3.connect(db_name)
+    cur = conn.cursor()
+    create_table = """ CREATE TABLE IF NOT EXISTS table1(
+                        ID integer PRIMARY KEY,
+                        title text NOT NULL,
+                        link text NOT NULL,
+                        date text NOT NULL,
+                        author text NOT NULL,
+                        reply text NOT NULL,
+                        content text NOT NULL
+                        ); """
+    cur.execute(create_table)
+    for i in posts:
+        cur.execute("insert into table1 (title, link, date, author, reply, content) values (?, ?, ?, ?, ?, ?)",
+            (i['title'], i['link'], i['date'], i['author'], i['reply'], i['content']))
+    conn.commit()
+    conn.close()
+
+##############################
+# save data into excel       #
+# param: posts -> posts data #
+##############################
+def Save2Excel(posts):
+    titles = [entry['title'] for entry in posts]
+    links = [entry['link'] for entry in posts]
+    dates = [entry['date'] for entry in posts]
+    authors = [entry['author'] for entry in posts]
+    replies = [entry['reply'] for entry in posts]
+    contents = [entry['content'] for entry in posts]
+    df = DataFrame({
+        'title':titles,
+        'link':links,
+        'date': dates,
+        'author':authors,
+        'reply': replies,
+        'content': contents
+        })
+    df.to_excel('data.xlsx', sheet_name='sheet1', index=False, columns=['title', 'link', 'date', 'author', 'reply', 'content'])
--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import Mobile01Crawler as MCrawler
+import time
 
 def main():
-    #topic_list = get_all_topic('https://www.mobile01.com/index.php')
-    '''
-    for each in topic_list:
-        print(each)
-        print(get_total_page_num(each[0]))'''
-    print(GetTotalPageNum('topiclist.php?f=749'))
+    # topic_dict = MCrawler.GetAllTopic('https://www.mobile01.com/index.php')
+    topic_dict = MCrawler.ReadTopic()
 
-    page = 1
+    print(u'Which topic would you want to crawl?')
+    idx = input('Input the number in front of the topic (see topic_list.txt):').lstrip().rstrip()
+    total_page_num = int(MCrawler.GetTotalPageNum(topic_dict[idx][0]))
 
-    posts = GetPosts(page, 'topiclist.php?f=749')
+    print(u'Topic {{ {} }} has {} pages in total.'.format(topic_dict[idx][1], total_page_num))
+    page_want_to_crawl = input(u'How many pages do you want to crawl? ').lstrip().rstrip()
+    if page_want_to_crawl == '' or not page_want_to_crawl.isdigit() or int(page_want_to_crawl) <= 0:
+            print(u'EXIT')
+    else:
+        page_want_to_crawl = min(int(page_want_to_crawl), total_page_num)
 
-    print(len(posts))
+        start = time.time()
 
-    all_post_content = GetArticles(posts)
-    
-    print(len(all_post_content))
-    
-    print('==============')
+        posts = MCrawler.GetPosts(page_want_to_crawl, topic_dict[idx][0])
+        print(u'{} posts in total.'.format(len(posts)))
+
+        posts_data = MCrawler.GetArticles(posts)
+
+        print(u'Finish. Spend {} seconds on crawling.'.format(time.time()-start))
+
+        ans = input('Save to database? [yes/no]:')
+        if ans.lower() == 'yes':
+            MCrawler.Save2DB('data.db', posts_data)
+        ans = input('Save to excel? [yes/no]:')
+        if ans.lower() == 'yes':
+            MCrawler.Save2Excel(posts_data)
+            
 
 if __name__ == '__main__':
     main()
